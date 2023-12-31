96fc0befc87e06e463d34442a932da975f54f0d1
ims0s <sorour2001@gmail.com>
2023-12-31 20:16:15 +0200

    home page finished

---
+++ js/home.js
@@ -0,0 +1,13 @@
+const currentUserName=document.getElementById('current-user')
+const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+
+
+if (!currentUser){
+    location.assign('/')
+}
+
+currentUserName.innerText=currentUser.name
+document.getElementById('logout').addEventListener('click',()=>{
+    localStorage.removeItem('currentUser')
+    location.assign('/')
+})
