024ff6778c83e7987fe6971bffd937137622e11e
ims0s <sorour2001@gmail.com>
2023-12-31 20:22:45 +0200

    fix the same bug

--- js/home.js
+++ js/home.js
@@ -3,11 +3,11 @@ const currentUser = JSON.parse(localStorage.getItem('currentUser'));
 
 
 if (!currentUser){
-    location.assign('../../')
+    location.assign('../index.html')
 }
 
 currentUserName.innerText=currentUser.name
 document.getElementById('logout').addEventListener('click',()=>{
     localStorage.removeItem('currentUser')
-    location.assign('../../')
+    location.assign('../index.html')
 })
